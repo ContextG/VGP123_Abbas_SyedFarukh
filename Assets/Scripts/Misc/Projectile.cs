6200a5b1a5deeb880cf2f0f93fa3d7d037684d81
Farukh Abbas <CattCaller168@gmail.com>
2022-02-08 10:21:10 -0500

    Update

--- Assets/Scripts/Misc/Projectile.cs
+++ Assets/Scripts/Misc/Projectile.cs
@@ -6,19 +6,48 @@ public class Projectile : MonoBehaviour
 {
 
     public float Speed = 4.5f;
+    private float direction;
+    private bool hit;
 
-    
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
     private void Update()
     {
         
-        transform.position += -transform.right * Time.deltaTime * Speed;
+        if (hit) return;
+        float movementSpeed = Speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0, 0);
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        Destroy(gameObject);
-
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("Hit");
     }
 
+    public void SetDirection(float _direction)
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false); 
+    }
 }
